f45c731213750bcc5984550307b345fa5b66918d
tdanbo <tobiasd@playdead.com>
2023-03-16 13:42:10 +0100

    .

--- src/main.py
+++ src/main.py
@@ -1,3 +1,23 @@
 import os
 
+from PySide2.QtWidgets import *
+from PySide2.QtGui import *
+from PySide2.QtCore import *
+
 print("Hello World 4")
+
+
+class GuiTest(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.layout = QVBoxLayout()
+        self.widget = QLabel("Working!")
+        self.layout.addWidget(self.widget)
+        self.setLayout(self.layout)
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    gui = GuiTest()
+    gui.show()
+    app.exec_()
