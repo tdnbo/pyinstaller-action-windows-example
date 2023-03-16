d000913a561cc0dead8cd45d2246f2d99cf142c4
tdanbo <tobiasd@playdead.com>
2023-03-16 14:24:32 +0100

    .

--- src/main.py
+++ src/main.py
@@ -1,5 +1,21 @@
-import time
+import os
 
-for i in range(10):
-    print("Hello World 4")
-    time.sleep(1)
+from PySide2.QtWidgets import *
+from PySide2.QtGui import *
+from PySide2.QtCore import *
+
+
+class GuiTest(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.layout = QVBoxLayout()
+        self.widget = QLabel("Working!")
+        self.layout.addWidget(self.widget)
+        self.setLayout(self.layout)
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    gui = GuiTest()
+    gui.show()
+    app.exec_()
